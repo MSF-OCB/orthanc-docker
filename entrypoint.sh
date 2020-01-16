1841c577121c5a28a8c59b7bcda86ac7c073594f
RdN <141248+R-VdP@users.noreply.github.com>
2020-01-16 12:48:17 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ CMD="$@"
 sed -e "s/%orthanc_http_password%/${ORTHANC_HTTP_PASSWORD}/" /etc/orthanc/orthanc.json.tmpl > /etc/orthanc/orthanc.json
 
 # Original entrypoint was "Orthanc"
-Orthanc /etc/orthanc
+Orthanc ${CMD}
 
